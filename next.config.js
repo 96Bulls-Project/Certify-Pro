ee609b15013a051dfd38a6d0d488f71b88b5a419
Jose Naime <josep.naimeg@gmail.com>
2023-06-02 12:52:13 -0600

    Netlify plugin added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
+    NEXT_PUBLIC_REDIS_URI: process.env.NEXT_PUBLIC_REDIS_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+  }
 }
 
 module.exports = nextConfig
